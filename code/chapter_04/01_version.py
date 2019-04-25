de04f181d6da24a4fa1f948385f02ff0977a7c52
tarbo <mellitus4u@gmail.com>
2019-04-25 14:02:31 -0400

    updated gitignore file to hide copyrighted items

--- code/chapter_04/01_version.py
+++
@@ -1,4 +0,0 @@
-# check PIL and Pillow version numbers
-import PIL
-print('Pillow Version:', PIL.__version__)
-print('PIL Version:', PIL.VERSION)
--- code/chapter_10/04_force_channel_ordering.py
+++
@@ -1,8 +0,0 @@
-# force a channel ordering
-from keras import backend
-# force channels-first ordering
-backend.set_image_dim_ordering('th')
-print(backend.image_data_format())
-# force channels-last ordering
-backend.set_image_dim_ordering('tf')
-print(backend.image_data_format())
